f1b1152ad346c44a85eab55cd218178f5b4f7305
root <root@185-20-227-151.xen.vps.regruhosting.ru>
2015-10-08 21:00:44 +0300

    ignoring

--- yii2app/config/db.php
+++ yii2app/config/db.php
@@ -2,7 +2,7 @@
 
 return [
     'class' => 'yii\db\Connection',
-    'dsn' => 'mysql:host=localhost;dbname=cms',
+    'dsn' => 'mysql:host=localhost;dbname=client_viktor',
     'username' => 'palmengi_clients',
     'password' => 'mazikin8926152',
     'charset' => 'utf8',
