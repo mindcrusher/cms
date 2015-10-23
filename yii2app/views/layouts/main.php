713108b74affe7d18cad42296324cd4e656c3db1
Willy Snow <sid@feelthedrive.ru>
2015-10-23 19:25:01 +0300

    mobile version

--- yii2app/views/layouts/main.php
+++ yii2app/views/layouts/main.php
@@ -20,95 +20,98 @@ AppAsset::register($this);
 
 <?php $this->beginBody() ?>
     <div class="container">
-
-        <main id="panel">
-            <div class="col-xs-12">
-                <?php
-                NavBar::begin(['brandLabel' => 'Taggerd', 'id' => 'navbar-main']);
-                echo Nav::widget([
-                    'items' => [
-                        ['label' => 'Обратная связь', 'url' => ['/site/contact'], 'linkOptions' => [
-                            'class' =>  'showModalButton hidden-xs',
-                            'data-target' => '#pending-form',
-                            'data-toggle' => 'modal',
-                            'title' => 'Обратная связь',
-                        ]],
-                        ['label' => 'Онлайн заявка', 'url' => ['/site/pending'], 'linkOptions' => [
-                            'class' =>  'showModalButton hidden-xs',
-                            'data-target' => '#pending-form',
-                            'data-toggle' => 'modal',
-                            'title' => 'Заявка онлайн',
-                        ]],
-                        ['label' => 'Обратная связь', 'url' => ['/site/pending'], 'linkOptions' => [
-                            'class' =>  'visible-xs'
-                        ]],
-                        ['label' => 'Онлайн заявка', 'url' => ['/site/pending'], 'linkOptions' => [
-                            'class' =>  'visible-xs'
-                        ]],
-                        ['label' => 'Калькулятор', 'url' => ['/calc/default/index']],
-                    ],
-                    'options' => [
-                        'class' => 'navbar-nav navbar-right'
-                    ],
-                ]);
-                NavBar::end();
-                ?>
-            </div>
-            <div class="row">
-                <?php
-                $hasMenu = !empty(Yii::$app->controller->menu);
-                if($hasMenu) {
-                ?>
-                <div class="col-sm-3 hidden-xs">
+        <div class="row">
+            <main id="panel">
+                <div class="">
                     <?php
-                        echo Menu::widget(Yii::$app->controller->menu[4]['links']);
+                    NavBar::begin(['brandLabel' => 'Taggerd', 'id' => 'navbar-main']);
+                    echo Nav::widget([
+                        'items' => [
+                            ['label' => 'Обратная связь', 'url' => ['/site/contact'], 'linkOptions' => [
+                                'class' =>  'showModalButton hidden-xs',
+                                'data-target' => '#pending-form',
+                                'data-toggle' => 'modal',
+                                'title' => 'Обратная связь',
+                            ]],
+                            ['label' => 'Онлайн заявка', 'url' => ['/site/pending'], 'linkOptions' => [
+                                'class' =>  'showModalButton hidden-xs',
+                                'data-target' => '#pending-form',
+                                'data-toggle' => 'modal',
+                                'title' => 'Заявка онлайн',
+                            ]],
+                            ['label' => 'Обратная связь', 'url' => ['/site/pending'], 'linkOptions' => [
+                                'class' =>  'visible-xs'
+                            ]],
+                            ['label' => 'Онлайн заявка', 'url' => ['/site/pending'], 'linkOptions' => [
+                                'class' =>  'visible-xs'
+                            ]],
+                            ['label' => 'Калькулятор', 'url' => ['/calc/default/index']],
+                        ],
+                        'options' => [
+                            'class' => 'navbar-nav navbar-right'
+                        ],
+                    ]);
+                    NavBar::end();
                     ?>
-                </div>
-                <?php } ?>
-                <div class="col-xs-12 col-sm-<?=$hasMenu ? 9 : 12?>"><?=$content?></div>
-            </div>
-            <footer>
-                <div>
-                    <div class="row">
-                        <div class="col-sm-4">
-                            <?php foreach(app\models\Contacts::findAllActive() as $contact) { ?>
-                                <div><span class="<?=$contact->icon()?>" aria-hidden="true"></span> &nbsp;<?=$contact->formatted();?></div>
-                            <? } ?>
-                        </div>
-                        <div class="col-sm-4">
-                            <?php
-                            if(!empty(Yii::$app->controller->menu)) {
-                                echo Menu::widget(Yii::$app->controller->menu[5]['links']);
-                            }
-                            ?>
-                        </div>
-                        <div class="col-sm-4">somtehting else</div>
-                    </div>
-                </div>
-            </footer>
-        </main>
-        <div class="hidden-md">
-            <?php
-            echo raoul2000\widget\slideout\Slideout::widget([
-                'pluginOptions' => [
-                    'panel' =>  new yii\web\JsExpression("document.getElementById('panel')"),
-                    'menu' => new yii\web\JsExpression("document.getElementById('mobile_menu')"),
-                    'padding' => 256,
-                    'tolerance' => 70
-                ]
-            ]);
 
-            ?>
-            <nav id="mobile_menu">
-                <header>
-                    <span class="h4">Главное меню</span>
-                </header>
+                    <?php
+                    $hasMenu = !empty(Yii::$app->controller->menu);
+                    if($hasMenu) {
+                    ?>
+                    <div class="col-sm-3 hidden-xs">
+                        <?php
+                            echo Menu::widget(Yii::$app->controller->menu[4]['links']);
+                        ?>
+                    </div>
+                    <?php } ?>
+                    <div class="col-xs-12 col-sm-<?=$hasMenu ? 9 : 12?>"><?=$content?></div>
+
+                    <footer>
+                        <div>
+                            <div class="row">
+                                <div class="col-sm-4">
+                                    <?php foreach(app\models\Contacts::findAllActive() as $contact) { ?>
+                                        <div><span class="<?=$contact->icon()?>" aria-hidden="true"></span> &nbsp;<?=$contact->formatted();?></div>
+                                    <? } ?>
+                                </div>
+                                <div class="col-sm-4">
+                                    <?php
+                                    if(!empty(Yii::$app->controller->menu)) {
+                                        echo Menu::widget(Yii::$app->controller->menu[5]['links']);
+                                    }
+                                    ?>
+                                </div>
+                                <div class="col-sm-4">somtehting else</div>
+                            </div>
+                        </div>
+                    </footer>
+                </div>
+            </main>
+
+
+            <div class="hidden-md">
                 <?php
-                if(!empty(Yii::$app->controller->menu)) {
-                    echo Menu::widget(Yii::$app->controller->menu[4]['links']);
-                }
+                echo raoul2000\widget\slideout\Slideout::widget([
+                    'pluginOptions' => [
+                        'panel' =>  new yii\web\JsExpression("document.getElementById('panel')"),
+                        'menu' => new yii\web\JsExpression("document.getElementById('mobile_menu')"),
+                        'padding' => 256,
+                        'tolerance' => 70
+                    ]
+                ]);
+
                 ?>
-            </nav>
+                <nav id="mobile_menu">
+                    <header>
+                        <span class="h4">Главное меню</span>
+                    </header>
+                    <?php
+                    if(!empty(Yii::$app->controller->menu)) {
+                        echo Menu::widget(Yii::$app->controller->menu[4]['links']);
+                    }
+                    ?>
+                </nav>
+            </div>
         </div>
     </div>
 
